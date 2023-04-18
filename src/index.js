b55546994b61014ad2ae10f16c0886177abb2ddf
AboutGG <gabrielegiuliano3102@gmail.com>
2023-04-18 16:50:35 +0200

    now it works with url and by seraching the name of song

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const { Client, GatewayIntentBits, Attachment } = require('discord.js');
 const { joinVoiceChannel, createAudioResource, NoSubscriberBehavior, createAudioPlayer, generateDependencyReport } = require('@discordjs/voice');
 require('dotenv').config();
-const { video_basic_info, stream } = require('play-dl');
+const { video_basic_info, stream, yt_validate, search } = require('play-dl');
 
 const PREFIX = "!";
-var servers = {};
 
 const client = new Client({
   intents: [
@@ -16,48 +15,101 @@ const client = new Client({
   ],
 });
 
+client.on('messageCreate', async message => {
 
+  let args = splitString(message.content)
+
+  switch (args[0]) {
+    case "!play":
+      if (!message.member.voice?.channel) return message.channel.send('Connect to a Voice Channel')
+
+      const connection = joinVoiceChannel({
+        channelId: message.member.voice.channel.id,
+        guildId: message.guild.id,
+        adapterCreator: message.guild.voiceAdapterCreator
+      })
+
+      let streamData;
+
+      if (args[1].startsWith('https') && yt_validate(args[1]) === 'video'){
+        streamData = await stream(args[1])
+      }
+      else {
+        let yt_info = await search(args[1], {
+          limit: 1
+        }) 
+        streamData = await stream(yt_info[0].url)
+      }
+
+      let resource = createAudioResource(streamData.stream, {
+        inputType: streamData.type
+      })
+
+      let player = createAudioPlayer({
+        behaviors: {
+          noSubscriber: NoSubscriberBehavior.Play
+        }
+      })
+
+      player.play(resource)
+
+      connection.subscribe(player)
+      break;
+  }
+
+});
+/*
 client.on('messageCreate', async message => {
 
   if (message.content.startsWith('!play')) {
-      
-      if (!message.member.voice?.channel) return message.channel.send('Connect to a Voice Channel')
-      
-      const connection = joinVoiceChannel({
-          channelId: message.member.voice.channel.id,
-          guildId: message.guild.id,
-          adapterCreator: message.guild.voiceAdapterCreator
-      })
 
-      let args = message.content.split('play ')[1].split(' ')[0]
+    if (!message.member.voice?.channel) return message.channel.send('Connect to a Voice Channel')
 
-      let streamData = await play.stream(args)
-      
+    const connection = joinVoiceChannel({
+      channelId: message.member.voice.channel.id,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator
+    });
+
+    let args = message.content.split('play ')[1].split(' ')[0];
+    if (yt_validate(args)) {
+
+      let streamData = await stream(args)
+
       /*
       OR if you want to get info about youtube link and then stream it
       let yt_info = await play.video_info(args)
       console.log(yt_info.video_details.title) 
       let stream = await play.stream_from_info(yt_info)
-      */
+      
 
       let resource = createAudioResource(streamData.stream, {
-          inputType: streamData.type
+        inputType: streamData.type
       })
 
       let player = createAudioPlayer({
-          behaviors: {
-              noSubscriber: NoSubscriberBehavior.Play
-          }
+        behaviors: {
+          noSubscriber: NoSubscriberBehavior.Play
+        }
       })
-      
+
       player.play(resource)
 
       connection.subscribe(player)
+
+    }
+
   }
 })
-
+*/
 client.on("ready", () => {
   console.log(client.user.username)
 })
 
 client.login(process.env.TOKEN);
+
+function splitString(str) {
+  let splitStr = str.trim().split(/\s+/);
+  let result = [splitStr.shift(), splitStr.join(' ')];
+  return result;
+}
